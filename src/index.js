aa9e8e796a7f5dd448b46f84791860c966a3adf7
Joao208 <augustoj311@gmail.com>
2020-06-18 01:54:23 -0300

    bugs

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ const bodyParser = require('body-parser')
 const socketio = require('socket.io');
 const http = require('http');
 const cookieParser = require('cookie-parser');
-import * as Sentry from '@sentry/node';
 
 const app = express()
 
-Sentry.init({ dsn: 'https://598bf1f96aa44c638f360d19e8ed0074@o382729.ingest.sentry.io/5212042' });
 
 const server = http.Server(app);
 const io = socketio(server);
@@ -47,17 +45,6 @@ app.use(
   "/files",
   express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
 );
-app.use(Sentry.Handlers.requestHandler());
-app.use(Sentry.Handlers.errorHandler());
-app.use(Sentry.Handlers.errorHandler({
-  shouldHandleError(error) {
-    // Capture all 404 and 500 and 403 and 400 errors
-    if (error.status === 404 || error.status === 500 || error.status === 400 || error.status === 503) {
-      return true
-    }
-    return false
-  }
-}));
 
 require('./app/controllers/authController')(app)
 require('./app/controllers/publishController')(app)
