0b98385e45f812d69828e2517459f556a03a3fea
Joao <59159025+Joao208@users.noreply.github.com>
2020-07-23 03:12:51 -0300

    pay

--- src/app/controllers/CheckoutController.js
+++ src/app/controllers/CheckoutController.js
@@ -1,102 +1,19 @@
 const {
   Router
 } = require('express')
-import pagarme from 'pagarme'
-import User from '../models/user'
 const router = Router()
 
 router.use(authMiddleware)
 
-router.post('/checkout', async (req, res) => {
-  const {
-    card_hash,
-    save_card,
-    card_id,
-  } = req.body;
-
-  const address = {
-    zipcode: '02419070',
-    street: 'Rua Coronel Joaquim Ferreira de Souza',
-    street_number: '983',
-    neighborhood: 'Conjunto Residencial Santa Terezinha',
-    city: 'São Paulo',
-    state: 'SP'
-  }
-
-  try {
-
-    const customer = User.findById(req.userId)
-
-    let card;
-    if (card_id) {
-      card = await CreditCard.findOrFail(card_id);
-    }
-
-    const client = await pagarme.client.connect({
-      api_key: process.env.PAGARME_API_KEY,
-    });
-
-    const amount = 1;
-
-    const pagarmeTransaction = await client.transactions.create({
-      amount: parseInt(amount, 10),
-      ...(card_hash ? { card_hash } : { card_id: card.card_id }),
-      customer: {
-        name: customer.name,
-        email: customer.email,
-        country: 'br',
-        external_id: '1',
-        type: 'individual',
-        documents: [
-          {
-            type: 'cpf',
-            number: '43867587809',
-          },
-          {
-            type: 'rg',
-            number: '586336023',
-          },
-        ],
-        phone_numbers: [customer.phone],
-      },
-      billing: {
-        name: customer.name,
-        address: {
-          ...address,
-          country: 'br',
-        },
-      },
-      shipping: {
-        name: customer.name,
-        fee,
-        delivery_date: '2019-07-21',
-        expedited: false,
-        address: {
-          ...address,
-          country: 'br',
-        },
-      },
-      items: {
-        tangible:false
-      },
-    });
-
-    if (save_card && !card) {
-      const { card } = pagarmeTransaction;
-
-      await CreditCard.findOrCreate({
-        card_id: card.id,
-        number: `${card.first_digits}*********${card.last_digits}`,
-        holder_name: card.holder_name,
-        brand: card.brand,
-        expiration_date: card.expiration_date,
-      });
-    }
-
-    return res.json(transactions.toJSON());
-  } catch (err) {
-    return res.status(400).json(err);
-  }
+router.post('/create-payment-intent', async (req, res) => {
+  const stripe = require("stripe")("sk_test_51H7wkvGHhRYZj7pYmWxDTUBu6LpyAAwn0hVB1lrACKkURVCPZjEb6WCU3q7EEwdWXioaKxSFyLKTxhcWreKwxikM00uekeWZwa");
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 50,
+    currency: "brl"
+  });
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  });
 })
 
 module.exports = app => app.use(router)
