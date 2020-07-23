bf7f6ec11ec268a91e4d6fdbe6421c02e2fb7dc5
Joao <59159025+Joao208@users.noreply.github.com>
2020-07-23 20:06:04 -0300

    pay

--- src/app/controllers/CheckoutController.js
+++ src/app/controllers/CheckoutController.js
@@ -3,7 +3,7 @@ const {
 } = require('express')
 const router = Router()
 const authMiddleware = require('../middlewares/auth')
-const stripe = require("stripe")(process.env.SECURITY_KEY_STRIPE);
+const stripe = require("stripe")("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");
 router.use(authMiddleware)
 
 router.post('/create-payment-intent', async (req, res) => {
