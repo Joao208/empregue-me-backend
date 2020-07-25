e4a00d9c253503a6610c0cfa0585a91eae7a6f15
Joao <59159025+Joao208@users.noreply.github.com>
2020-07-25 04:41:57 -0300

    billing stripe

--- src/app/controllers/CheckoutController.js
+++ src/app/controllers/CheckoutController.js
@@ -15,5 +15,77 @@ router.post('/payment-intent', async (req, res) => {
     clientSecret: paymentIntent.client_secret
   });
 })
+router.post('/create-costumer', async (req, res) => {
+
+  const customer = await stripe.customers.create({
+    email: req.body.email,
+  });
+
+  // save the customer.id as stripeCustomerId
+  // in your database.
+
+  res.send({
+    customer
+  });
+
+})
+router.post('/create-subscription', async (req, res) => {
+  // Attach the payment method to the customer
+  try {
+    await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+      customer: req.body.customerId,
+    });
+  } catch (error) {
+    return res.status('402').send({
+      error: {
+        message: error.message
+      }
+    });
+  }
+
+  // Change the default invoice settings on the customer to the new payment method
+  await stripe.customers.update(
+    req.body.customerId, {
+      invoice_settings: {
+        default_payment_method: req.body.paymentMethodId,
+      },
+    }
+  );
+
+  // Create the subscription
+  const subscription = await stripe.subscriptions.create({
+    customer: req.body.customerId,
+    items: [{
+      price: 'price_HGd7M3DV3IMXkC'
+    }],
+    expand: ['latest_invoice.payment_intent'],
+  });
+
+  res.send(subscription);
+});
+router.post('/retry-invoice', async (req, res) => {
+  // Set the default payment method on the customer
+
+  try {
+    await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+      customer: req.body.customerId,
+    });
+    await stripe.customers.update(req.body.customerId, {
+      invoice_settings: {
+        default_payment_method: req.body.paymentMethodId,
+      },
+    });
+  } catch (error) {
+    // in case card_decline error
+    return res
+      .status('402')
+      .send({ result: { error: { message: error.message } } });
+  }
+
+  const invoice = await stripe.invoices.retrieve(req.body.invoiceId, {
+    expand: ['payment_intent'],
+  });
+  res.send(invoice);
+});
 
 module.exports = app => app.use(router)
